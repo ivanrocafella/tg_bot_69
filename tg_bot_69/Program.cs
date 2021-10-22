bdcdea506c27d28e266097e90508a0cb6f173ac2
Ivan Kobtsev <vanomc77@gmail.com>
2021-10-22 11:17:20 +0600

    Добавьте файлы проекта.

---
+++ tg_bot_69/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tg_bot_69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
